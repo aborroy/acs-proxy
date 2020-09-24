7e5f2fc5657e78c668331f1c9c029ec4794e2937
Angel Borroy <angel.borroy@alfresco.com>
2020-09-24 11:25:52 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# ACS Community Nginx Proxy
+
+An enhanced Web Proxy container for ACS Community deployment.
+
+## Environment variables
+
+| Name | Default | Description |
+| --- | --- | --- |
+| REPO_URL | `http://alfresco:8080` | Repository URL inside network. |
+| SHARE_URL | `http://share:8080` | Share URL inside network. |
+| SOLR_URL | `http://solr6:8983` | SOLR URL inside network. |
+| ACCESS_LOG | n/a | Set the `access_log` value. Set to `off` to switch off logging. |
+| PORT | `8080` | External port number for the Web Proxy |
+| PROTOCOL | `http` | External protocol for the Web Proxy: `http` or `https` |
+| ACA_ENABLED | `true` | Configure Web Proxy for Alfresco Content Application in context / when `true` |
+| ALFRESCO_ENABLED | `true` | Configure Web Proxy for Repository in context /alfresco when `true` |
+| SHARE_ENABLED | `true` | Configure Web Proxy for Share in context /share when `true` |
+| SOLR_ENABLED | `true` | Configure Web Proxy for SOLR in context /solr when `true`, protected by user/password |
+| SOLR_PROTECT | `true` | Disable the access to Repository SOLR REST APIs when `true`, so they cannot be accessed using the Web Proxy |
+
+
+## Examples
+
+Development environment only with **alfresco** and **solr** services.
+SOLR Web Console protected by user / password in `nginx.htpasswd` file.
+Repository SOLR REST APIs open to be used from Web Proxy.
+
+```yml
+proxy:
+    image: aborroy/acs-proxy:1.0.0
+    environment:
+      - ACA_ENABLED=false
+      - SHARE_ENABLED=false
+      - SOLR_PROTECT=false
+    depends_on:
+        - alfresco
+        - solr6
+    volumes:
+        - ./config/nginx.htpasswd:/etc/nginx/conf.d/nginx.htpasswd
+    ports:
+        - 8080:8080
+    links:
+        - alfresco
+        - solr6
+```
+
+SSL Environment without Alfresco Content Application.
+
+```yml
+proxy:
+    image: aborroy/acs-proxy:1.0.0
+    mem_limit: 128m
+    environment:
+      - ACA_ENABLED=false
+      - PORT=443
+      - PROTOCOL=https
+    depends_on:
+        - alfresco
+    volumes:
+        - ./config/nginx.htpasswd:/etc/nginx/conf.d/nginx.htpasswd
+        - ./config/cert/localhost.cer:/etc/nginx/localhost.cer
+        - ./config/cert/localhost.key:/etc/nginx/localhost.key
+    ports:
+        - 443:443
+    links:
+        - alfresco
+        - share
+        - solr6
+```        
---
+++ entrypoint.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+PORT="${PORT:-8080}"
+PROTOCOL="${PROTOCOL:-http}"
+ACA_ENABLED="${ACA_ENABLED:-true}"
+ALFRESCO_ENABLED="${ALFRESCO_ENABLED:-true}"
+SHARE_ENABLED="${SHARE_ENABLED:-true}"
+SOLR_ENABLED="${SOLR_ENABLED:-true}"
+SOLR_PROTECT="${SOLR_PROTECT:-true}"
+
+if [[ "$PROTOCOL" == "https" ]]; then
+  sed -i s%\#LISTEN%"listen *:$PORT ssl;\n\
+  \      ssl_certificate     /etc/nginx/localhost.cer;\n\
+  \      ssl_certificate_key /etc/nginx/localhost.key;\n\
+  \      ssl_ciphers         EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH;\n\
+  \      ssl_protocols       TLSv1.1 TLSv1.2;\n"%g /etc/nginx/nginx.conf
+else
+  sed -i s%\#LISTEN%"listen *:$PORT;"%g /etc/nginx/nginx.conf
+fi
+
+if [[ "$SOLR_PROTECT" == "true" ]]; then
+  sed -i s%\#SOLR_PROTECT%"location ~ ^(/.*/service/api/solr/.*)$ {return 403;}\n\
+  \      location ~ ^(/.*/s/api/solr/.*)$ {return 403;}\n\
+  \      location ~ ^(/.*/wcservice/api/solr/.*)$ {return 403;}\n\
+  \      location ~ ^(/.*/wcs/api/solr/.*)$ {return 403;}\n\
+  \      location ~ ^(/.*/proxy/alfresco/api/solr/.*)$ {return 403 ;}\n\
+  \      location ~ ^(/.*/-default-/proxy/alfresco/api/.*)$ {return 403;}\n"%g /etc/nginx/nginx.conf
+fi
+
+if [[ "$ACA_ENABLED" == "true" ]]; then
+  sed -i s%\#ACA_ENDPOINT%"location / {\n\
+  \        proxy_pass http://alfresco:8080;\n\
+  \      }"%g /etc/nginx/nginx.conf
+fi
+
+if [[ "$ALFRESCO_ENABLED" == "true" ]]; then
+  sed -i s%\#ALFRESCO_ENDPOINT%"location /alfresco/ {\n\
+  \        proxy_pass http://alfresco:8080;\n\
+  \      }"%g /etc/nginx/nginx.conf
+fi
+
+if [[ "$SHARE_ENABLED" == "true" ]]; then
+  sed -i s%\#SHARE_ENDPOINT%"location /share/ {\n\
+  \        proxy_pass http://share:8080;\n\
+  \      }"%g /etc/nginx/nginx.conf
+fi
+
+if [[ "$SOLR_ENABLED" == "true" ]]; then
+  sed -i s%\#SOLR_ENDPOINT%'location /solr/ {\
+  \        proxy_pass http://solr6:8983;\
+  \        auth_basic "Solr web console";\
+  \        auth_basic_user_file /etc/nginx/conf.d/nginx.htpasswd;\
+  \      }'%g /etc/nginx/nginx.conf
+fi
+
+if [[ $REPO_URL ]]; then
+  sed -i s%http:\/\/alfresco:8080%"$REPO_URL"%g /etc/nginx/nginx.conf
+fi
+
+if [[ $SHARE_URL ]]; then
+  sed -i s%http:\/\/share:8080%"$SHARE_URL"%g /etc/nginx/nginx.conf
+fi
+
+if [[ $SOLR_URL ]]; then
+  sed -i s%http:\/\/solr6:8983%"$SOLR_URL"%g /etc/nginx/nginx.conf
+fi
+
+if [[ $ACCESS_LOG ]]; then
+  sed -i s%\#ENV_ACCESS_LOG%"access_log $ACCESS_LOG;"%g /etc/nginx/nginx.conf
+fi
+
+nginx -g "daemon off;"
